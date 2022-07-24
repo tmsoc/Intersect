42a3e6cd11e352ca90662618e1f3e91b6e80da57
Tony Samaniego <tms.ocmp@gmail.com>
2022-07-24 14:02:50 -0700

    Created descriptive home/welcome pages

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,13 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!!"
+    user = {'username': "Tony"}
+    return render_template('index.html', title="home", user=user)
+
+
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome.html', title="Welcome")
