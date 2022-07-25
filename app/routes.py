9aea28e3297a0aabf2224b00b598273b281864e2
Tony Samaniego <tms.ocmp@gmail.com>
2022-07-24 17:09:14 -0700

    Implemented User and created DB

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,24 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': "Tony"}
-    return render_template('index.html', title="home", user=user)
+    return render_template('index.html', title="Home", user=user)
 
 
 @app.route('/welcome')
 def welcome():
     return render_template('welcome.html', title="Welcome")
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
